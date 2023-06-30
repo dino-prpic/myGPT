2835c61a7b0b6ff55d19b39daabcf6ebd948b8a9
dark14lord <93191676+dark14lord@users.noreply.github.com>
2023-06-30 16:05:01 +0200

    addedApi
    v1.0

---
+++ api/app.py
@@ -0,0 +1,59 @@
+import requests as requests
+from flask import Flask, request
+from dotenv import load_dotenv
+import time
+import os
+app = Flask(__name__)
+import subprocess
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/query', methods=['GET', 'POST'])
+def test():
+
+
+    question =request.json
+    print(question)
+
+    # answer = get_answer(question)
+    # print(answer)
+    # url = 'http://192.168.1.201:3000/pushAnswer'
+    #
+    # data = {'answer': 'blabla'}
+    #
+    # requests.request("POST", url, data=data)
+    # print('done')
+
+    return 'success'
+
+def get_answer(question):
+    load_dotenv('C:/Users/Ivor/Documents/GitHub/privateGPT/.env')
+
+    f = open("question.txt", "w")
+    f.write(question)
+    f.close()
+
+    command='start cmd /K python C:/Users/Ivor/Documents/GitHub/privateGPT/privateGPT.py'
+
+    subprocess.Popen(command,stdin=subprocess.PIPE, shell=True)
+
+    print('done')
+
+   # f = open("answer.txt", "r")
+
+    #answer=f.readline()
+    answer="blabla"
+    f.close()
+
+
+    return answer
+
+
+if __name__ == '__main__':
+
+    app.run('0.0.0.0', 5000, debug=True)
+
